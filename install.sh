b3956bf24ea9141820dcd8f246286b0722755666
fenrir <fenrir@Lycoris.(none)>
2012-05-20 14:09:30 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #Dropboxをインストールして
 #~/Dropboxにdotfilesを置いて管理すると他のPCからでも同じ設定が共有できる
 
-this_dir = `pwd`
+$this_dir = `pwd`
 ln -s $this_dir/vim/.vimrc ~/.vimrc
 ln -s $this_dir/zsh/.zshrc ~/.zshrc
 ln -s $this_dir/screen/.screenrc ~/.screenrc
