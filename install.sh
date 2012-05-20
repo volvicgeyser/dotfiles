0c27e520a4a91283f1138c3e0c68dcf0ea611cc5
fenrir <fenrir@Lycoris.(none)>
2012-05-20 13:41:02 +0900

    install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#シンボリックリンクを貼る
+
+#Dropboxをインストールして
+#~/Dropboxにdotfilesを置いて管理すると他のPCからでも同じ設定が共有できる
+
+this_dir = `pwd`
+ln -s $this_dir/vim/.vimrc ~/.vimrc
+ln -s $this_dir/zsh/.zshrc ~/.zshrc
+ln -s $this_dir/screen/.screenrc ~/.screenrc
+ln -s $this_dir/emacs/.emacs ~/.emacs
+ln -s $this_dir/vimperator/_vimperatorrc ~/_vimperatorrc
